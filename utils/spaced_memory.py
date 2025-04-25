375d61645b5994574e651af2547c1da6f1fa3e47
George <geousa@Georges-MacBook-Air.local>
2025-04-25 21:17:02 +0300

    Обновление проекта AniAI

--- utils/spaced_memory.py
+++ utils/spaced_memory.py
@@ -11,6 +11,13 @@ def add_to_memory(user_id, word):
     if word not in user_memory[user_id]:
         user_memory[user_id].append(word)
 
+def update_word_memory(user_id, word):
+    """Обновить или добавить слово в память пользователя"""
+    if user_id not in user_memory:
+        user_memory[user_id] = []
+    if word in user_memory[user_id]:
+        user_memory[user_id].remove(word)
+    user_memory[user_id].append(word)
+
 def reset_memory(user_id):
     user_memory[user_id] = []
-
